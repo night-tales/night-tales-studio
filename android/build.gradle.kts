88b97e94ab88fa4322f15e674c313977b540abdf
night-tales <night26tales@gmail.com>
2026-08-25 23:50:19 +0200

    build(android): align Java and Kotlin JVM targets

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,8 +3,26 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-android { namespace = "com.hakayat.studio"; compileSdk = 35
-    defaultConfig { applicationId = "com.hakayat.studio"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
+android {
+    namespace = "com.hakayat.studio"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.hakayat.studio"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = "21"
+    }
 }
 
 dependencies {

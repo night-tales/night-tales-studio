260448f0be3b0e7bccdc84d91533c4e832ba97d0
night-tales <night26tales@gmail.com>
2026-08-25 23:55:01 +0200

    fix(android): apply Kotlin Compose compiler plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.jvm") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
     id("io.ktor.plugin") version "3.0.3" apply false
 }
